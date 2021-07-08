6e59dc7d43520dd885cf4f8ec5b209555506591b
kanu-e <chukwuemekakanu@gmail.com>
2021-07-08 16:55:55 -0500

    set state using hooks

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const  Accordion =({items})=>{
+    const [activeIndex, setActiveIndex]= useState(null);
     const onTitleClick = (index)=>{
-        console.log('question clicked', index)
+        console.log(setActiveIndex(index));
     }
     const renderedItems = items.map((item, index)=>{
         return(<React.Fragment key= {item.question}>
@@ -19,6 +20,7 @@ const  Accordion =({items})=>{
     })
     return <div className="ui styled accordion">
                 {renderedItems}
+                <h1>{activeIndex}</h1>
             </div>
 }
 
