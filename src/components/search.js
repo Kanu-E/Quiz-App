0fdbb96aa96fb212e4e77384d02e0a9b2447b9f9
kanu-e <chukwuemekakanu@gmail.com>
2021-09-06 10:36:14 -0500

    debounced term

--- src/components/search.js
+++ src/components/search.js
@@ -5,31 +5,34 @@ import axios from 'axios'
 const Search =()=> {
 
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
-   
+
+    useEffect(() => {
+        const timerId = setTimeout(() =>{
+            setDebouncedTerm(term)
+        }, 500)
+        return ()=>{
+            clearTimeout(timerId)
+        }
+    
+    }, [term]);
 
     useEffect(() => {
        const search =  async () =>{
-          const {data} =  await axios.get('https://en.wikipedia.org/w/api.php',{
-            params:{
-                action: "query", 
-                list : "search",
-                origin: "*",
-                format:"json",
-                srsearch: term,
-            }
-           })
-         setResults(data.query.search)
-        //  console.log(data)
-       }
-       const timeout = setTimeout(() =>{
-        if (term) search()
-       }, 500);
-       
-       return ()=>{
-        clearTimeout(timeout)
+            const {data} =  await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action: "query", 
+                    list : "search",
+                    origin: "*",
+                    format:"json",
+                    srsearch: debouncedTerm,
+                }
+            })
+         setResults(data.query.search);
        };
-    }, [term])
+            if (debouncedTerm) search();
+    },[debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
