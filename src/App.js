a2d1cfde1452e382eebb52e6470c285c4789284e
kanu-e <chukwuemekakanu@gmail.com>
2021-09-06 10:42:39 -0500

    scafold dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Accordion from './components/Accordion'
 import Search from './components/search'
+import Dropdown from './components/dropdown'
 // const items = [{title: 'What is React?' , content: 'React is a frontent javascript framework'},{title: "Why use React?", content :"React is a favourite JS library among engineers"}, {title:"bb", content: "jj"}]
 
 
@@ -8,7 +9,8 @@ const App = () => {
   return (
     <div className="App">
       {/* <Accordion items = {items}/> */}
-      <Search/>
+      {/* <Search/> */}
+      <Dropdown/>
     </div>
   );
 }
