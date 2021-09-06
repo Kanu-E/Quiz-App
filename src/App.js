e8b37c4cb4e6355b3621418c453050fa872f560c
kanu-e <chukwuemekakanu@gmail.com>
2021-09-06 07:49:29 -0500

    set up search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Accordion from './components/Accordion'
-
-const items = [{title: 'What is React?' , content: 'React is a frontent javascript framework'},{title: "Why use React?", content :"React is a favourite JS library among engineers"}, {title:"bb", content: "jj"}]
+// import Accordion from './components/Accordion'
+import Search from './components/search'
+// const items = [{title: 'What is React?' , content: 'React is a frontent javascript framework'},{title: "Why use React?", content :"React is a favourite JS library among engineers"}, {title:"bb", content: "jj"}]
 
 
 const App = () => {
   return (
     <div className="App">
-      <Accordion items = {items}/>
+      {/* <Accordion items = {items}/> */}
+      <Search/>
     </div>
   );
 }
