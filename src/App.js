daf3aeb2f6c2a7ec70262be82007f1ef1ec4faa8
kanu-e <chukwuemekakanu@gmail.com>
2021-09-06 11:23:59 -0500

    dropdown state setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import Accordion from './components/Accordion'
 import Search from './components/search'
 import Dropdown from './components/dropdown'
 // const items = [{title: 'What is React?' , content: 'React is a frontent javascript framework'},{title: "Why use React?", content :"React is a favourite JS library among engineers"}, {title:"bb", content: "jj"}]
+const options = [
+  {
+    label: 'The color is Red?',
+    value: "red"
+  },
+  {
+    label: 'The color is Blue?',
+    value: "blue"
+  },
+  {
+    label: 'The color is Green?',
+    value: "green"
+  },
+]
+
 
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0])
+
   return (
     <div className="App">
       {/* <Accordion items = {items}/> */}
       {/* <Search/> */}
-      <Dropdown/>
+      <Dropdown 
+        options={options} 
+        selected={selected}
+        onSelectedChange= {setSelected}
+      />
     </div>
   );
 }
--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,10 +1,27 @@
 import React from 'react'; 
 
-const Dropdown =()=>{
-
+const Dropdown =({options, selected, onSelectedChange})=>{
+    const renderedOptions = options.map((option)=>{
+        return (        
+            <div key = {option.value} className = "item"> 
+                {option.label}
+            </div>
+    );
+});
     return (
-        <div>Dropdown</div>
-    )
-}
+        <div className ="ui form">
+            <div className = "field">
+                <label className = "label">Select a color</label>
+            </div>
+            <div className ="ui selection dropdown visible active">
+                <i className="dropdown icon"></i>
+                <div className = "text"> {selected.label}</div>
+                <div className = "menu visible transition">
+                    {renderedOptions}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Dropdown; 
