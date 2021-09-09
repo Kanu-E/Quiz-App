1285ed8952deb6606e59979cba7fcda9617eb11d
kanu-e <chukwuemekakanu@gmail.com>
2021-09-09 10:51:54 -0500

    set ref to ref

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,11 +1,14 @@
-import React, {useState, useEffect} from 'react'; 
+import React, {useState, useEffect, useRef} from 'react'; 
 
 
 
 const Dropdown =({options, selected, onSelectedChange})=>{
-    const [open, setOpen] = useState(false)
+
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
         useEffect(()=>{
-            document.body.addEventListener('click',()=>{
+            document.body.addEventListener('click',(event)=>{
+                console.log(event.target)
                 setOpen(false)
             },{capture: true});
         },[])
@@ -24,7 +27,7 @@ const Dropdown =({options, selected, onSelectedChange})=>{
         );
     });
     return (
-        <div className ="ui form">
+        <div ref = {ref} className ="ui form">
             <div className = "field">
                 <label className = "label">Select a color</label>
             </div>
