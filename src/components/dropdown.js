b64cfaff7cc2ac0972be1c49ceb41537abea0499
kanu-e <chukwuemekakanu@gmail.com>
2021-09-09 10:48:18 -0500

    set open to false when body is clicked

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -5,10 +5,10 @@ import React, {useState, useEffect} from 'react';
 const Dropdown =({options, selected, onSelectedChange})=>{
     const [open, setOpen] = useState(false)
         useEffect(()=>{
-        document.body.addEventListener('click',()=>{
-            console.log('Clicked')
-        });
-    },[])
+            document.body.addEventListener('click',()=>{
+                setOpen(false)
+            });
+        },[])
     const renderedOptions = options.map((option)=>{
         if (option.value === selected.value) {
             return null
