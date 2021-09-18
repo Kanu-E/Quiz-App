572d9e33b529677ccce79016dc46652baff5b533
kanu-e <chukwuemekakanu@gmail.com>
2021-09-18 11:18:33 -0500

    create route

--- src/App.js
+++ src/App.js
@@ -1,43 +1,79 @@
 import React, {useState} from 'react';
-// import Accordion from './components/Accordion'
+import Accordion from './components/Accordion';
 import Search from './components/search'
-// import Dropdown from './components/dropdown'
+import Dropdown from './components/dropdown'
 import Translate from './components/Translate'
-// const items = [{title: 'What is React?' , content: 'React is a frontent javascript framework'},{title: "Why use React?", content :"React is a favourite JS library among engineers"}, {title:"bb", content: "jj"}]
+import Route from './components/Route'
 
-// const options = [
-//   {
-//     label: 'The color is Red?',
-//     value: "red"
-//   },
-//   {
-//     label: 'The color is Blue?',
-//     value: "blue"
-//   },
-//   {
-//     label: 'The color is Green?',
-//     value: "green"
-//   },
-// ]
+const items = [{title: 'What is React?' , content: 'React is a frontent javascript framework'},{title: "Why use React?", content :"React is a favourite JS library among engineers"}, {title:"bb", content: "jj"}]
+
+const options = [
+  {
+    label: 'The color is Red?',
+    value: "red"
+  },
+  {
+    label: 'The color is Blue?',
+    value: "blue"
+  },
+  {
+    label: 'The color is Green?',
+    value: "green"
+  },
+]
+
+// const showAccordion =()=>{
+//   if (window.location.pathname === "/") {
+//     return <Accordion items = {items}/>
+//   }
+// }
+// const showList =()=>{
+//   if (window.location.pathname === "/list") {
+//     return <Search/>
+//   }
+// }
+
+// const dropdown =()=>{
+//   if (window.location.pathname === "/dropdown") {
+//     return <Dropdown/>
+//   }
+// }
+
+// const translate =()=>{
+//   if (window.location.pathname === "/translate") {
+//     return <Translate/>
+//   }
+// }
 
 
 
 const App = () => {
-  // const [selected, setSelected] = useState(options[0]);
+  const [selected, setSelected] = useState(options[0]);
   // const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div className="App">
-      {/* <Accordion items = {items}/> */}
-      {/* <Search/> */}
-      {/* <button onClick={()=>setShowDropdown(!showDropdown)}> Toggle </button> */}
-      {/* {showDropdown?
-      <Dropdown 
-        options={options} 
-        selected={selected}
-        onSelectedChange= {setSelected}
-      />:null} */}
-      <Translate/>
+      <Route path="/">
+        <Accordion items = {items}/>
+      </Route>
+
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <Dropdown 
+          label = "select a color"
+          options={options} 
+          selected={selected}
+          onSelectedChange= {setSelected}
+        />
+      </Route>
+
+      <Route path="/translate">
+        <Translate/>
+      </Route>
+
     </div>
   );
 }
--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,6 +4,18 @@ import axios from 'axios'
 
 const Convert = ({language, text})=>{
     const [translated, setTranlated] = useState('')
+    // const [Text, setText] = useState('')
+    const [debounceText, setDebounceText] = useState(text)
+
+    useEffect(()=>{
+        const timerId = setTimeout(()=>{
+            setDebounceText(text);
+        }, 500)
+        return ()=>{
+            clearTimeout(timerId);
+        };
+    },[text])
+
     useEffect(() => {
         const doTranslate = async ()=>{
             const {data} = await axios.post(
@@ -11,7 +23,7 @@ const Convert = ({language, text})=>{
                 {},
                 {
                     params: {
-                        q: text,
+                        q: debounceText,
                         target:language.value,
                         key:'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
                     },
@@ -19,10 +31,14 @@ const Convert = ({language, text})=>{
             );
             setTranlated(data.data.translations[0].translatedText)
         };
-        // doTranslate()
-    },[language, text])
-    console.log(translated)
-    return <div></div>
+        doTranslate()
+    },[language, debounceText])
+    
+    return (
+        <div>
+            <h3>{translated}</h3>
+        </div>
+    )
 };
 
 export default Convert
